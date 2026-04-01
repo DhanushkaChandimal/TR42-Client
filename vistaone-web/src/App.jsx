b30841d961de88f3d2f02350f1f244c33e11cc24
Dhanushka <dhanushkamcr@gmail.com>
2026-04-01 03:07:54 -0500

    Initialize work order page

--- vistaone-web/src/App.jsx
+++ vistaone-web/src/App.jsx
@@ -1,5 +1,6 @@
 import Login from './components/Login'
 import Dashboard from './components/Dashboard'
+import WorkOrder from './components/WorkOrder'
 import { Route, Routes, Navigate } from 'react-router-dom'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
             <Route path="/" element={<Navigate to="/login" replace />} />
             <Route path="/login/*" element={<Login />} />
             <Route path="/dashboard/*" element={<Dashboard />} />
+            <Route path="/workorder/*" element={<WorkOrder />} />
             <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
     )
