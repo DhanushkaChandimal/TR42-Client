3c5db8b14186af39992cbe5edcf7f46e833e48c0
Dhanushka <dhanushkamcr@gmail.com>
2026-04-16 12:59:10 -0500

    Add auto-pan to marker in map

--- vistaone-web/src/components/MapPicker.jsx
+++ vistaone-web/src/components/MapPicker.jsx
@@ -1,4 +1,11 @@
-import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  useMapEvents,
+  useMap,
+} from "react-leaflet";
+import { useEffect } from "react";
 import "leaflet/dist/leaflet.css";
 
 function LocationMarker({ markerPos, setMarkerPos }) {
@@ -10,11 +17,21 @@ function LocationMarker({ markerPos, setMarkerPos }) {
   return markerPos ? <Marker position={markerPos} /> : null;
 }
 
+function AutoPanToMarker({ markerPos }) {
+  const map = useMap();
+  useEffect(() => {
+    if (markerPos && Array.isArray(markerPos) && markerPos.length === 2) {
+      map.setView(markerPos, map.getZoom(), { animate: true });
+    }
+  }, [markerPos, map]);
+  return null;
+}
+
 export default function MapPicker({ markerPos, setMarkerPos, height = 300 }) {
   return (
     <MapContainer
       center={markerPos || [39.8283, -98.5795]} // Center of US
-      zoom={5}
+      zoom={markerPos ? 13 : 5}
       style={{ height, width: "100%", marginBottom: 16 }}
       scrollWheelZoom={true}
     >
@@ -22,6 +39,7 @@ export default function MapPicker({ markerPos, setMarkerPos, height = 300 }) {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <AutoPanToMarker markerPos={markerPos} />
       <LocationMarker markerPos={markerPos} setMarkerPos={setMarkerPos} />
     </MapContainer>
   );
