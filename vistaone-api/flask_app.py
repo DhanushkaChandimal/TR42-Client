60edaa3c635a93dccc3923246a79aa23af45d90c
Dhanushka <dhanushkamcr@gmail.com>
2026-05-08 01:25:23 -0500

    Move load_dotenv function top to load environment variables before importing app

--- vistaone-api/flask_app.py
+++ vistaone-api/flask_app.py
@@ -1,9 +1,10 @@
 import os
 from dotenv import load_dotenv
-from app import create_app
-from app.extensions import db
 
 load_dotenv()
+
+from app import create_app
+from app.extensions import db
 
 config_name = os.getenv("FLASK_CONFIG")
 app = create_app(config_name)
