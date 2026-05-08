28f28896a2a0810308190a35f2f82ec518e3fd85
Dhanushka <dhanushkamcr@gmail.com>
2026-05-08 18:57:34 -0500

    Remove app.run for deployment

--- vistaone-api/flask_app.py
+++ vistaone-api/flask_app.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()  # Not needed for deployment on Render
 
 from app import create_app
 from app.extensions import db
@@ -9,5 +9,6 @@ from app.extensions import db
 config_name = os.getenv("FLASK_CONFIG")
 app = create_app(config_name)
 
-if __name__ == "__main__":
-    app.run(debug=app.config.get("DEBUG", False))
+# The following block is not needed for deployment on Render
+# if __name__ == "__main__":
+#     app.run(debug=app.config.get("DEBUG", False))
